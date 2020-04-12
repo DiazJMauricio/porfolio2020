c004343416cc1254c4158dd0bfbc291b9f88fbff
Diaz J. Mauricio <ryuma.diaz@gmail.com>
2020-04-11 23:49:19 -0300

    Updates

---
+++ precache-manifest.47f80bd1209d0d0e51312d515c364b40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb71f1a430b2945a090c90bdc6919c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d8122d0f883573434f9",
+    "url": "/static/css/main.4a56441a.chunk.css"
+  },
+  {
+    "revision": "072df928b37798fd3840",
+    "url": "/static/js/2.83da993b.chunk.js"
+  },
+  {
+    "revision": "0d8122d0f883573434f9",
+    "url": "/static/js/main.975cc2cc.chunk.js"
+  },
+  {
+    "revision": "416cc1c79b8c22940a8c",
+    "url": "/static/js/runtime~main.7302273d.js"
+  }
+]);
